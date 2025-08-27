3e58bff455cb2fc8e43bb423ead0d9e0cf099c95
mariyamnavila <bibimariyamnavila@gmail.com>
2025-08-27 18:04:46 +0600

    added wishlist in dashboard

---
+++ src/Component/WishListItem/WishListItem.jsx
@@ -0,0 +1,26 @@
+import { RxCrossCircled } from "react-icons/rx";
+import { AddToCart } from "../GadgetDetails/storage";
+
+const WishListItem = ({wishListItem}) => {
+    const { product_id, product_image, price, product_title, description } = wishListItem;
+    return (
+        <div className="bg-white p-8 rounded-2xl flex justify-between items-center mb-6">
+            <div className="flex items-center">
+                <div>
+                    <img className="w-[274px] h-[192px] rounded-xl mr-8" src={product_image} alt="" />
+                </div>
+                <div className="space-y-4">
+                    <p className="text-2xl font-semibold">{product_title}</p>
+                    <p className="text-xl text-[#6b6b6f]"><span className="text-black  font-semibold">Description: </span>{description}</p>
+                    <p className="text-2xl font-semibold">Price: ${price}</p>
+                    <button onClick={() => { AddToCart(product_id)}} className="py-3 px-6 bg-[#9036da] text-white text-xl rounded-full">Add to Cart</button>
+                </div>
+            </div>
+            <div>
+                <RxCrossCircled className="text-red-600 text-[36px]" />
+            </div>
+        </div>
+    );
+};
+
+export default WishListItem;
--- src/Component/Wishlist/Wishlist.jsx
+++ src/Component/Wishlist/Wishlist.jsx
@@ -1,8 +1,25 @@
-
+import { useEffect, useState } from "react";
+import { useLoaderData } from "react-router-dom";
+import { getWishList } from "../GadgetDetails/storage";
+import WishListItem from "../WishListItem/WishListItem";
 const Wishlist = () => {
+    const allGadget = useLoaderData();
+    const [wishlist,setWishList ] = useState([])
+     useEffect(()=>{
+        const WishList = getWishList();
+        const actualWishList = allGadget.filter(gadget=> WishList.includes(gadget['product_id']));
+        setWishList(actualWishList)
+    },[])
     return (
-        <div>
-            <h3>dadad</h3>
+        <div className="mb-52">
+            <div className="flex justify-between items-center my-10">
+                <p className="text-2xl font-bold">WishList</p>
+            </div>
+            <div>
+                {
+                    wishlist.map((wishListItem,idx)=><WishListItem key={idx} wishListItem={wishListItem}></WishListItem>)
+                }
+            </div>
         </div>
     );
 };
